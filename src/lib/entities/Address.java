85b0f809a4af8b3234352c6af6c1b352d85dd647
root <root@Gilles.home>
2020-05-27 09:38:39 -0400

    Attributs and Methods hae been completed

--- src/lib/entities/Address.java
+++ src/lib/entities/Address.java
@@ -1,5 +1,72 @@
 package lib.entities;
 
 public class Address {
+	
+	 int buildingNumber;
+	 String street, apt, city, state;
+	 int zipCode;
+	 User user;
+	 
+	 
+	public Address(int buildingNumber, String street, String apt, String city, String state, int zipCode, User user) {
+		super();
+		this.buildingNumber = buildingNumber;
+		this.street = street;
+		this.apt = apt;
+		this.city = city;
+		this.state = state;
+		this.zipCode = zipCode;
+		this.user = user;
+	}
+	
+	
+	public int getBuildingNumber() {
+		return buildingNumber;
+	}
+	public String getStreet() {
+		return street;
+	}
+	public String getApt() {
+		return apt;
+	}
+	public String getCity() {
+		return city;
+	}
+	public String getState() {
+		return state;
+	}
+	public int getZipCode() {
+		return zipCode;
+	}
+	public User getUser() {
+		return user;
+	}
+	public void setBuildingNumber(int buildingNumber) {
+		this.buildingNumber = buildingNumber;
+	}
+	public void setStreet(String street) {
+		this.street = street;
+	}
+	public void setApt(String apt) {
+		this.apt = apt;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+	public void setZipCode(int zipCode) {
+		this.zipCode = zipCode;
+	}
+	public void setUser(User user) {
+		this.user = user;
+	}
+	 
+	 
+	 
+	 
 
+	 
+	 
 }
--- src/lib/entities/Book.java
+++ src/lib/entities/Book.java
@@ -1,5 +1,38 @@
 package lib.entities;
 
 public class Book {
+	
+	String title;
+	int year;
+	String author;
+	
+	
+	public Book(String title, int year, String author) {
+		super();
+		this.title = title;
+		this.year = year;
+		this.author = author;
+	}
+	
+	public String getTitle() {
+		return title;
+	}
+	public int getYear() {
+		return year;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public void setYear(int year) {
+		this.year = year;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	
+	
 
 }
